0d691eb3744b5a9b003d987728313dbdd1f34478
Rolando del Valle <delvallefstm96@gmail.com>
2022-12-29 18:40:42 -0500

    iniciando repositorio para proyecto integrador

---
+++ src/main/java/com/ejercicio/springbootapirest/dto/UserDto.java
@@ -0,0 +1,41 @@
+package com.ejercicio.springbootapirest.dto;
+
+public class UserDto {
+    private String name, email, lastName;
+
+    public UserDto() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", apellido='" + lastName + '\'' +
+                '}';
+    }
+}
